9b44c9b288de71bd2f517158d51abfba44db477f
takasys1215 <sazanami.pg3373@gmail.com>
2021-06-12 22:39:16 +0900

    production用の設定を追加

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: isProd ? '/kakeibo' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
 }
